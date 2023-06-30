4697747a71e346e6fbde9c91630932bba5f8fc9f
Oskar Pawłowski <oskar7799@gmail.com>
2023-06-30 19:33:52 +0200

    Podstawowa logika do obsługi Task
    Dodano:
    - klasę Task do obsługi zadań
    - klasę TasksList do obsługi listy zadań

---
+++ Manage_tasks/Task.cs
@@ -0,0 +1,36 @@
+﻿namespace Manage_tasks
+{
+    /// <summary>
+    /// Klasa obsługująca zadanie.
+    /// </summary>
+    public class Task
+    {
+        private int Id { get; } //do przyszłej obsługi bazy danych 
+        
+        //kolejne właściwości do dodania
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        //
+
+        /// <summary>
+        /// Podstawowy konstruktor do tworzenia zadania.
+        /// </summary>
+        /// <param name="TaskName">Parametr przedstawiający nazwę zadania.</param>
+        /// <param name="TaskDescription">Parametr przedstawiający opis zadania.</param>
+        public Task(string TaskName, string TaskDescription)
+        {
+            this.TaskName = TaskName;
+            this.TaskDescription = TaskDescription;
+        }
+        /// <summary>
+        /// Metoda klasy wypisująca szczegóły wybranego zadania.
+        /// </summary>
+        public void TaskDetails()
+        {
+            //opis pozostaje do zmiany po ustaleniu wyglądu
+            Console.WriteLine(TaskName + " " + TaskDescription);
+        }
+        
+        
+    }
+}
---
+++ Manage_tasks/TasksList.cs
@@ -0,0 +1,64 @@
+﻿namespace Manage_tasks
+{
+    /// <summary>
+    /// Klasa obsługująca listę zadań.
+    /// </summary>
+    public class TasksList
+    {
+        private int Id { get; } //do przyszłej obsługi bazy danych
+
+
+        //kolejne właściwości do dodania jeśli zajdzie taka potrzeba
+        public List<Task> Tasks { get; set;}
+        public string TasksListName { get; set; }
+        //
+        /// <summary>
+        /// Podstawowy konstruktor do tworzenia listy zadań.
+        /// </summary>
+        /// <param name="TasksListName">Nazwa listy zadań.</param>
+        public TasksList(string TasksListName)
+        {
+            Tasks = new List<Task>();
+            this.TasksListName = TasksListName;
+        }
+        /// <summary>
+        /// Metoda dodająca obiekt klasy Task do listy zadań.
+        /// </summary>
+        /// <param name="NewTask">Obiekt klasy Task</param>
+        public void AddTask(Task NewTask)
+        {           
+            Tasks.Add(NewTask);
+        }
+        /// <summary>
+        /// Metoda zwracająca szczegóły wszystkich zadań w liście zadań.
+        /// </summary>
+        public void DisplayTasksDetails()
+        {
+            foreach (var Task in Tasks) 
+            {
+                Task.TaskDetails();
+            }
+        }
+        /// <summary>
+        /// Metoda obsługująca usuwanie zadania.
+        /// </summary>
+        /// <param name="NewTask"></param>
+        public void RemoveTask(Task NewTask)
+        { 
+            Tasks.Remove(NewTask); 
+        }
+        /// <summary>
+        /// Metoda do wybierania zadania z listy zadań.
+        /// </summary>
+        /// <param name="IndexOfTask">Indeks z listy Tasks</param>
+        /// <returns>Zwraca zmienną klasy Task</returns>
+        public Task PickTask(int IndexOfTask)
+        {
+            Task Task = Tasks[IndexOfTask];
+
+            return Task;
+        }
+            
+     
+    }
+}
