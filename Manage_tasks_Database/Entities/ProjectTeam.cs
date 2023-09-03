4e51142b1c7ea79c2b04d71b67f8879bc03076f3
RafalPujer <rafal.pujer00@gmail.com>
2023-09-03 20:32:39 +0200

    Added database project and entities.

---
+++ Manage_tasks_Database/Entities/ProjectTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_tasks_Database.Entities;
+
+internal class ProjectTeam
+{
+    public Guid ProjectId { get; set; }
+
+    public Guid TeamId { get; set; }
+}
+
