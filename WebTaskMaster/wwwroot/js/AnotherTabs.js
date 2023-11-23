ea4d0a22ea5e49a38ce52c9e9f735db0d4068830
Vladyslav Bezuhlyi <129013750+SparkPino@users.noreply.github.com>
2023-11-23 04:10:56 +0100

    super-first-step

---
+++ WebTaskMaster/wwwroot/js/AnotherTabs.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var tabs = document.querySelectorAll(".tabs_wrapNew ul li");
+    var info = document.querySelector(".info");
+    var task = document.querySelector(".task");
+
+    // Инициализация при загрузке страницы
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            tabs.forEach((tab) => {
+                tab.classList.remove("active");
+            })
+            tab.classList.add("active");
+            var tabval = tab.getAttribute("data-tabs");
+
+            info.style.display = "none";
+            task.style.display = "none";
+
+            if (tabval == "info") {
+                info.style.display = "block";
+            }
+            else if (tabval == "task") {
+                task.style.display = "block";
+            }
+        })
+    });
+
+    // Сделаем активной вкладку "Project Information" после обработки кликов
+    document.querySelector("[data-tabs='info']").click();
+});
