7e283c9c6459d4dac7dafb76cac92e48cca9d6d1
Vladyslav Bezuhlyi <129013750+SparkPino@users.noreply.github.com>
2023-11-26 06:05:10 +0100

    progressbar + start accordion description added

--- WebTaskMaster/wwwroot/js/AnotherTabs.js
+++ WebTaskMaster/wwwroot/js/AnotherTabs.js
@@ -27,3 +27,30 @@
     // Сделаем активной вкладку "Project Information" после обработки кликов
     document.querySelector("[data-tabs='info']").click();
 });
+
+const circularProgress = document.querySelectorAll(".circular-progress");
+
+Array.from(circularProgress).forEach((progressBar) => {
+    const progressValue = progressBar.querySelector(".percentage");
+    const innerCircle = progressBar.querySelector(".inner-circle");
+    let startValue = 0,
+        endValue = Number(progressBar.getAttribute("data-percentage")),
+        speed = 30,
+        progressColor = progressBar.getAttribute("data-progress-color");
+
+    const progress = setInterval(() => {
+        startValue++;
+        progressValue.textContent = `${startValue}%`;
+        progressValue.style.color = `${progressColor}`;
+
+        innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+            "data-inner-circle-color"
+        )}`;
+
+        progressBar.style.background = `conic-gradient(${progressColor} ${startValue * 3.6
+            }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+        if (startValue === endValue) {
+            clearInterval(progress);
+        }
+    }, speed);
+});
