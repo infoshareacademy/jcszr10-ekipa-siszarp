80af26822a774c5e23d23ed362546906cb86d423
RafalPujer <rafal.pujer00@gmail.com>
2023-12-12 18:30:50 +0100

    Initial setup for tests project

---
+++ Manage_tasks_Biznes_Logic_Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using Manage_tasks_Biznes_Logic.Service;
+global using AutoFixture;
+global using AutoMapper;
+global using Manage_tasks_Database.Context;
+global using Microsoft.Data.Sqlite;
+global using Microsoft.EntityFrameworkCore;
+global using System.Data.Common;
