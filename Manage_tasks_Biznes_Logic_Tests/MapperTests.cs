9f5390f49b66260d46c3d26b5acddfd7d1e59369
RafalPujer <rafal.pujer00@gmail.com>
2023-12-18 18:07:35 +0100

    Added Tests for Mapper Profiles

---
+++ Manage_tasks_Biznes_Logic_Tests/MapperTests.cs
@@ -0,0 +1,20 @@
+﻿using Manage_tasks_Biznes_Logic.Profiles;
+
+namespace Manage_tasks_Biznes_Logic_Tests;
+
+public class MapperTests
+{
+    [Fact]
+    public void ValidateMapperProfiles()
+    {
+        // Arrange
+        var config = new MapperConfiguration(cfg => cfg.AddMaps(typeof(TeamProfile).Assembly));
+        var mapper = config.CreateMapper();
+
+        // Act
+        var action = mapper.ConfigurationProvider.AssertConfigurationIsValid;
+
+        // Assert
+        action.Should().NotThrow();
+    }
+}
